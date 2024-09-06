d71f1b7973bbb822d93c3a63b9e013c4101e55b9
Jil-Patel05 <jilpatel2105@gmail.com>
2024-09-06 23:15:29 +0530

    hey buddy

--- backend/Helpers/Libraries/sendEmail.js
+++ backend/Helpers/Libraries/sendEmail.js
@@ -15,7 +15,6 @@ const sendEmail = async (mailOptions) => {
     tls: { rejectUnauthorized: false },
     secure: true,
   });
-  console.log(transporter);
   // return await new Promise((resolve, reject) => {
   //   transporter.sendMail(mailOptions, (err, info) => {
   //     if (err) {
@@ -26,13 +25,14 @@ const sendEmail = async (mailOptions) => {
   //     }
   //   });
   // });
+  console.log("currently above this code");
   try {
     let info = await transporter.sendMail(mailOptions);
   } catch (error) {
     console.log("error aa gy he re");
     console.log(error);
   }
-  console.log(`Message send :${info}`);
+  console.log(`Message send`);
 };
 
 module.exports = sendEmail;
